84b6a00b28d51944d7cd39a1afa497eef5285fa0
Jessica Meeker <jesmeeker@gmail.com>
2023-01-16 09:58:57 -0600

    deduced do_GET function to call get_all_or_single function and added method_mapper

--- request_handler.py
+++ request_handler.py
@@ -2,10 +2,32 @@ import json
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from views import get_all_metals, get_single_metal, create_metal, delete_metal, update_metal, get_all_orders, get_single_order, create_order, delete_order, update_order, get_all_sizes, get_single_size, create_size, delete_size, update_size, get_all_styles, get_single_style, create_style, delete_style, update_style
 
+method_mapper = {
+    'orders': {'single': get_single_order, 'all': get_all_orders},
+    'metals': {'single': get_single_metal, 'all': get_all_metals},
+    'sizes': {'single': get_single_size, 'all': get_all_sizes},
+    'styles': {'single': get_single_style, 'all': get_all_styles}
+}
+
 
 class HandleRequests(BaseHTTPRequestHandler):
     """Controls the functionality of any GET, PUT, POST, DELETE requests to the server
     """
+
+    def get_all_or_single(self, resource, id):
+        if id is not None:
+            response = method_mapper[resource]["single"](id)
+
+            if response is not None:
+                self._set_headers(200)
+            else:
+                self._set_headers(404)
+                response = ''
+        else:
+            self._set_headers(200)
+            response = method_mapper[resource]["all"]()
+
+        return response
 
     def parse_url(self, path):
         # Just like splitting a string in JavaScript. If the
@@ -30,77 +52,82 @@ class HandleRequests(BaseHTTPRequestHandler):
 
     def do_GET(self):
         """Handles GET requests to the server """
-        response = {}  # Default response
 
-        # Parse the URL and capture the tuple that is returned
+        response = None
         (resource, id) = self.parse_url(self.path)
-
-        if resource == "metals":
-            if id is not None:
-                response = get_single_metal(id)
-
-                if response is not None:
-                    self._set_headers(200)
-
-                else:
-                    self._set_headers(404)
-                    response = {"message": "That metal does not exist."}
-
-            else:
-                self._set_headers(200)
-                response = get_all_metals()
-
-        elif resource == "orders":
-            if id is not None:
-                response = get_single_order(id)
-
-                if response is not None:
-                    self._set_headers(200)
-
-                else:
-                    self._set_headers(404)
-                    response = {"message": "That order was never placed, or was cancelled."}
-            else:
-                self._set_headers(200)
-                response = get_all_orders()
-
-        elif resource == "styles":
-            if id is not None:
-                response = get_single_style(id)
-
-                if response is not None:
-                    self._set_headers(200)
-
-                else:
-                    self._set_headers(404)
-                    response = {"message": "That style does not exist."}
-            
-            else:
-                self._set_headers(200)
-                response = get_all_styles()
-
-        elif resource == "sizes":
-            if id is not None:
-                response = get_single_size(id)
-
-                if response is not None:
-                    self._set_headers(200)
-
-                else:
-                    self._set_headers(404)
-                    response = {"message": "That size does not exist."}
-            else:
-                self._set_headers(200)
-                response = get_all_sizes()
-
-        else:
-            self._set_headers(404)
-            response = ""
-
+        response = self.get_all_or_single(resource, id)
         self.wfile.write(json.dumps(response).encode())
+        # response = {}  # Default response
+
+        # # Parse the URL and capture the tuple that is returned
+        # (resource, id) = self.parse_url(self.path)
+
+        # if resource == "metals":
+        #     if id is not None:
+        #         response = get_single_metal(id)
+
+        #         if response is not None:
+        #             self._set_headers(200)
+
+        #         else:
+        #             self._set_headers(404)
+        #             response = {"message": "That metal does not exist."}
+
+        #     else:
+        #         self._set_headers(200)
+        #         response = get_all_metals()
+
+        # elif resource == "orders":
+        #     if id is not None:
+        #         response = get_single_order(id)
+
+        #         if response is not None:
+        #             self._set_headers(200)
+
+        #         else:
+        #             self._set_headers(404)
+        #             response = {"message": "That order was never placed, or was cancelled."}
+        #     else:
+        #         self._set_headers(200)
+        #         response = get_all_orders()
+
+        # elif resource == "styles":
+        #     if id is not None:
+        #         response = get_single_style(id)
+
+        #         if response is not None:
+        #             self._set_headers(200)
+
+        #         else:
+        #             self._set_headers(404)
+        #             response = {"message": "That style does not exist."}
+
+        #     else:
+        #         self._set_headers(200)
+        #         response = get_all_styles()
+
+        # elif resource == "sizes":
+        #     if id is not None:
+        #         response = get_single_size(id)
+
+        #         if response is not None:
+        #             self._set_headers(200)
+
+        #         else:
+        #             self._set_headers(404)
+        #             response = {"message": "That size does not exist."}
+        #     else:
+        #         self._set_headers(200)
+        #         response = get_all_sizes()
+
+        # else:
+        #     self._set_headers(404)
+        #     response = ""
+
+        # self.wfile.write(json.dumps(response).encode())
 
     def do_POST(self):
-        
+
         content_len = int(self.headers.get('content-length', 0))
         post_body = self.rfile.read(content_len)
 
@@ -124,7 +151,7 @@ class HandleRequests(BaseHTTPRequestHandler):
                 self._set_headers(400)
                 new_data = {
                     "message": f'{"metalId is required" if "metalId" not in post_body else ""} {"sizeId is required" if "sizeId" not in post_body else ""} {"styleId is required" if "styleId" not in post_body else ""}'
-                        }
+                }
 
         if resource == "metals":
             if "metal" in post_body and "price" in post_body:
@@ -134,7 +161,7 @@ class HandleRequests(BaseHTTPRequestHandler):
                 self._set_headers(400)
                 new_data = {
                     "message": f'{"metal is required" if "metal" not in post_body else ""} {"price is required" if "price" not in post_body else ""}'
-                        }
+                }
 
         if resource == "sizes":
             if "carets" in post_body and "price" in post_body:
@@ -144,7 +171,7 @@ class HandleRequests(BaseHTTPRequestHandler):
                 self._set_headers(400)
                 new_data = {
                     "message": f'{"carets is required" if "carets" not in post_body else ""} {"price is required" if "price" not in post_body else ""}'
-                        }
+                }
 
         if resource == "styles":
             if "style" in post_body and "price" in post_body:
@@ -154,7 +181,7 @@ class HandleRequests(BaseHTTPRequestHandler):
                 self._set_headers(400)
                 new_data = {
                     "message": f'{"style is required" if "style" not in post_body else ""} {"price is required" if "price" not in post_body else ""}'
-                        }
+                }
 
         # Encode the new animal and send in response
         self.wfile.write(json.dumps(new_data).encode())
